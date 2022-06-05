ee9c5abef9820f7d1198f6638cc19ee618e72a89
Abhishek Muralidharan <75883645+abhishek68@users.noreply.github.com>
2022-06-05 23:09:33 +0530

    hi

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const feedback = require('./models/feedback')
 const franchise = require('./models/franchise')
 const bcrypt = require('bcrypt');
 const user  = require("./models/user")
+const hi = "hi";
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs');
 app.use(express.static(__dirname + '/public'));
